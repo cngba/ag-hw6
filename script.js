8bb71d424abf914ba35c1109c5503942f08b4ab6
Nguyen Ba Cong <congngba.21@gmail.com>
2025-03-25 16:21:51 +0700

    allows multiple video

--- script.js
+++ script.js
@@ -1,33 +1,41 @@
-
 let processor = {
-    timerCallback: function() {
-        if (this.video.paused || this.video.ended) {
+    videos: [],
+    canvases: [],
+    contexts: [],
+
+    timerCallback: function(index) {
+        if (this.videos[index].paused || this.videos[index].ended) {
             return;
         }
-        this.computeFrame();
+        this.computeFrame(index);
         let self = this;
-        setTimeout(function () { self.timerCallback(); }, 0);
+        setTimeout(function () { self.timerCallback(index); }, 0);
     },
 
     doLoad: function() {
-        this.video = document.getElementById("video");
-        this.can = document.getElementById("canvas");
-        this.ctx = this.can.getContext("2d");
-        let self = this;
-        this.video.addEventListener("play", function() {
-                self.width = self.video.videoWidth;
-                self.height = self.video.videoHeight;
-                self.timerCallback();
-            }, false
-        );
+        this.videos = document.getElementsByClassName("video");
+        this.canvases = document.getElementsByClassName("canvas");
+
+        for (let i = 0; i < this.videos.length; i++) {
+            this.contexts[i] = this.canvases[i].getContext("2d");
+
+            let self = this;
+            this.videos[i].addEventListener("play", function() {
+                self.timerCallback(i);
+            }, false);
+        }
     },
 
-    computeFrame: function() {
-        this.ctx.drawImage(this.video, 0, 0, this.width, this.height);
-        let frame = this.ctx.getImageData(0, 0, this.width, this.height);
+    computeFrame: function(index) {
+        let video = this.videos[index];
+        let ctx = this.contexts[index];
+        let width = video.videoWidth;
+        let height = video.videoHeight;
+
+        ctx.drawImage(video, 0, 0, width, height);
+        let frame = ctx.getImageData(0, 0, width, height);
         frame = this.detectEdge(frame);
-        this.ctx.putImageData(frame, 0, 0);
-        return;
+        ctx.putImageData(frame, 0, 0);
     },
 
     detectEdge: function(frame) {
@@ -38,29 +46,20 @@ let processor = {
 
         // Convert to grayscale
         for (let i = 0; i < length; i += 4) {
-            let r = frame.data[i];
-            let g = frame.data[i + 1];
-            let b = frame.data[i + 2];
-
-            let gray = 0.3 * r + 0.59 * g + 0.11 * b;
-
-            frame.data[i] = gray;
-            frame.data[i + 1] = gray;
-            frame.data[i + 2] = gray;
+            let gray = 0.3 * frame.data[i] + 0.59 * frame.data[i + 1] + 0.11 * frame.data[i + 2];
+            frame.data[i] = frame.data[i + 1] = frame.data[i + 2] = gray;
         }
 
         // Sobel edge detection
         for (let y = 1; y < h - 1; ++y) {
             for (let x = 1; x < w - 1; ++x) {
                 let i = (y * w + x) << 2;
-                let gx = -frame.data[i - 4 - (w<<2)] - 2 * frame.data[i - 4] - frame.data[i - 4 + (w<<2)] +
-                          frame.data[i + 4 - (w<<2)] + 2 * frame.data[i + 4] + frame.data[i + 4 + (w<<2)];
-                let gy = -frame.data[i - 4 - (w<<2)] - 2 * frame.data[i - (w<<2)] - frame.data[i + 4 - (w<<2)] +
-                          frame.data[i - 4 + (w<<2)] + 2 * frame.data[i + (w<<2)] + frame.data[i + 4 + (w<<2)];
+                let gx = -frame.data[i - 4 - (w << 2)] - 2 * frame.data[i - 4] - frame.data[i - 4 + (w << 2)]
+                        + frame.data[i + 4 - (w << 2)] + 2 * frame.data[i + 4] + frame.data[i + 4 + (w << 2)];
+                let gy = -frame.data[i - 4 - (w << 2)] - 2 * frame.data[i - (w << 2)] - frame.data[i + 4 - (w << 2)]
+                        + frame.data[i - 4 + (w << 2)] + 2 * frame.data[i + (w << 2)] + frame.data[i + 4 + (w << 2)];
                 let mag = Math.sqrt(gx * gx + gy * gy) > 100 ? 255 : 0;
-                ans.data[i + 0] = mag;
-                ans.data[i + 1] = mag;
-                ans.data[i + 2] = mag;
+                ans.data[i] = ans.data[i + 1] = ans.data[i + 2] = mag;
                 ans.data[i + 3] = 255;
             }
         }
@@ -72,4 +71,3 @@ let processor = {
 document.addEventListener("DOMContentLoaded", () => {
     processor.doLoad();
 });
-
